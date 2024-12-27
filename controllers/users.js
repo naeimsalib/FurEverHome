0c6a9f0c444f9e5bcdb6a2181597bdaf2533d3de
naeimsalib <naeimsalib@yahoo.com>
2024-12-26 20:28:48 -0500

    Add User info updating functionlity, to allow user to update and change there account information, like name, email and password

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 const ensureSignedIn = require('../middleware/ensure-signed-in');
 
 // GET /users - List all users (admin only)
@@ -19,6 +20,37 @@ router.delete('/:id', ensureSignedIn, async (req, res) => {
   }
   await User.findByIdAndDelete(req.params.id);
   res.redirect('/users');
+});
+
+// GET /users/edit - Show form to edit user information
+router.get('/edit', ensureSignedIn, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    res.render('users/edit', { title: 'Edit Profile', user });
+  } catch (err) {
+    console.error(err);
+    res.redirect('/error');
+  }
+});
+
+// POST /users/edit - Update user information
+router.post('/edit', ensureSignedIn, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    user.name = req.body.name;
+    user.email = req.body.email;
+
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(req.body.password, salt);
+    }
+
+    await user.save();
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.redirect('/error');
+  }
 });
 
 module.exports = router;
